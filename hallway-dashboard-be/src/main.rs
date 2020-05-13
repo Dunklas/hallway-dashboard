f6e56b3486500df9bd31b0444d4f5c28690a3408
Dunklas <dunklas@gmail.com>
2020-05-13 21:35:27 +0200

    Fetch public transport (#34)
    Fetch public transport departures from Trafiklab and store results in an S3 bucket

--- hallway-dashboard-be/src/main.rs
+++ hallway-dashboard-be/src/main.rs
@@ -4,13 +4,14 @@ use std::str::FromStr;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use simple_logger;
-use log::{self, info, warn};
+use log::{self, warn};
 use serde::{Deserialize, Serialize};
 use rusoto_core::{Region, ByteStream};
 use rusoto_s3::{S3, S3Client, PutObjectRequest};
 use tokio::runtime::Runtime;
 
 mod weather;
+mod public_transport;
 
 #[derive(Deserialize)]
 struct EmptyEvent {}
@@ -31,15 +32,20 @@ fn handle_request(_e: EmptyEvent, _c: Context) -> Result<EmptyOutput, HandlerErr
     let storage_bucket_name = env::var("STORAGE_BUCKET").unwrap();
     let s3_client = S3Client::new(aws_region);
 
-    let dark_sky_api_key = env::var("DARK_SKY_API_KEY").unwrap();
-    Runtime::new()
-        .expect("Failed to create Tokio runtime")
-        .block_on(get_and_store_weather(dark_sky_api_key, &s3_client, storage_bucket_name));
+    let mut run_time = Runtime::new()
+        .expect("Failed to create Tokio runtime");
 
+    let dark_sky_api_key = env::var("DARK_SKY_API_KEY").unwrap();
+    run_time.block_on(get_and_store_weather(dark_sky_api_key, &s3_client, &storage_bucket_name));
+
+    let trafiklab_api_key = env::var("TRAFIKLAB_API_KEY").unwrap();
+    let stop_id = env::var("PUBLIC_TRANSPORT_STOP").unwrap();
+    let direction = env::var("PUBLIC_TRANSPORT_DIRECTION").ok();
+    run_time.block_on(get_and_store_public_transport(trafiklab_api_key, stop_id, direction, &s3_client, &storage_bucket_name));
     Ok(EmptyOutput{})
 }
 
-async fn get_and_store_weather(dark_sky_api_key: String, s3_client: &S3Client, bucket_name: String) {
+async fn get_and_store_weather(dark_sky_api_key: String, s3_client: &S3Client, bucket_name: &String) {
     let weather = match weather::get_weather_forecast(dark_sky_api_key) {
         Ok(weather) => weather,
         Err(e) => {
@@ -49,12 +55,37 @@ async fn get_and_store_weather(dark_sky_api_key: String, s3_client: &S3Client, b
     };
     let bytes = match serde_json::to_vec(&weather) {
         Ok(bytes) => bytes,
-        Err(e) => {
+        Err(_e) => {
             warn!("Failed to convert weather to bytes");
             return;
         }
     };
-    let put_req = create_put_request(bucket_name, "weather".to_string(), bytes);
+    let put_req = create_put_request(bucket_name.to_string(), "weather".to_string(), bytes);
+    match s3_client.put_object(put_req).await {
+        Ok(res) => res,
+        Err(e) => {
+            warn!("Failed to write to S3: {}", e);
+            return;
+        }
+    };
+}
+
+async fn get_and_store_public_transport(trafiklab_api_key: String, stop_id: String, direction: Option<String>, s3_client: &S3Client, bucket_name: &String) {
+    let public_transport = match public_transport::get_public_transport(trafiklab_api_key, stop_id, direction) {
+        Ok(public_transport) => public_transport,
+        Err(e) => {
+            warn!("Failed to get public transport times: {}", e);
+            return;
+        }
+    };
+    let bytes = match serde_json::to_vec(&public_transport) {
+        Ok(bytes) => bytes,
+        Err(_e) => {
+            warn!("Failed to convert weather to bytes");
+            return;
+        }
+    };
+    let put_req = create_put_request(bucket_name.to_string(), "public_transport".to_string(), bytes);
     match s3_client.put_object(put_req).await {
         Ok(res) => res,
         Err(e) => {
---
+++ hallway-dashboard-be/src/public_transport.rs
@@ -0,0 +1,193 @@
+extern crate chrono;
+extern crate ureq;
+
+use chrono::{DateTime, Utc, TimeZone, LocalResult, NaiveDateTime, NaiveDate, NaiveTime};
+use chrono_tz::Europe::Stockholm;
+use serde::{Serialize, Deserialize};
+use std::fmt;
+
+#[derive(Deserialize, Serialize, Debug)]
+pub struct Departure {
+    number: String,
+    stop: String,
+    time: DateTime<Utc>,
+    #[serde(rename = "realTime")]
+    real_time: Option<DateTime<Utc>>,
+    direction: String
+}
+
+#[derive(Deserialize, Debug)]
+struct ExternalDeparture {
+    stop: String,
+    time: String,
+    #[serde(rename = "rtTime")]
+    rt_time: Option<String>,
+    date: String,
+    #[serde(rename = "rtDate")]
+    rt_date: Option<String>,
+    direction: String,
+    #[serde(rename = "transportNumber")]
+    transport_number: String
+}
+
+#[derive(Deserialize, Debug)]
+struct Response {
+    #[serde(rename = "Departure")]
+    departure: Vec<ExternalDeparture>
+}
+
+pub struct PublicTransportError {
+    message: String
+}
+
+impl fmt::Display for PublicTransportError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "PublicTransportError: {}", self.message)
+    }
+}
+impl fmt::Debug for PublicTransportError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{{ file: {}, line: {} }}", file!(), line!())
+    }
+}
+
+fn server_url() -> String {
+    #[cfg(not(test))]
+    let url = String::from("https://api.resrobot.se/v2");
+    #[cfg(test)]
+    let url = mockito::server_url();
+    return url;
+}
+
+pub fn get_public_transport(api_key: String, stop_id: String, direction: Option<String>) -> Result<Vec<Departure>, PublicTransportError> {
+    let raw_response = get_public_transport_via_http(api_key, stop_id, direction)?;
+    let response = match serde_json::from_slice::<Response>(raw_response.as_slice()) {
+        Ok(parsed) => parsed,
+        Err(_e) => {
+            return Err(PublicTransportError{
+                message: "Failed while parsing public transport API response".to_string()
+            });
+        }
+    };
+    let mut transformed_response = Vec::<Departure>::new();
+    for dep in response.departure {
+        let time = match to_datetime(dep.date, dep.time) {
+            Some(time) => time,
+            None => {
+                return Err(PublicTransportError{
+                    message: "Failed while parsing public transport API response".to_string()
+                });
+            }
+        };
+        let optional_real_time = to_datetime_from_optional(dep.rt_date, dep.rt_time);
+        transformed_response.push(Departure{
+            number: dep.transport_number,
+            stop: dep.stop,
+            time: time,
+            real_time: optional_real_time,
+            direction: dep.direction
+        });
+    }
+    return Ok(transformed_response);
+}
+
+fn to_datetime_from_optional(date: Option<String>, time: Option<String>) -> Option<DateTime<Utc>> {
+    if date.is_none() || time.is_none() {
+        return None;
+    }
+    return to_datetime(date.unwrap(), time.unwrap());
+}
+
+fn to_datetime(date: String, time: String) -> Option<DateTime<Utc>> {
+    let date = match NaiveDate::parse_from_str(&date, "%Y-%m-%d") {
+        Ok(parsed) => parsed,
+        Err(_e) => {
+            return None;
+        }
+    };
+    let time = match NaiveTime::parse_from_str(&time, "%H:%M:%S") {
+        Ok(parsed) => parsed,
+        Err(_e) => {
+            return None;
+        }
+    };
+    let naive_datetime = NaiveDateTime::new(date, time);
+    let sweden_time = match Stockholm.from_local_datetime(&naive_datetime) {
+        LocalResult::Single(parsed) => parsed,
+        LocalResult::Ambiguous(_a, _b) => {
+            return None;
+        },
+        LocalResult::None => {
+            return None;
+        }
+    };
+    return Some(sweden_time.with_timezone(&Utc));
+}
+
+fn get_public_transport_via_http(api_key: String, stop_id: String, direction: Option<String>) -> Result<Vec::<u8>, PublicTransportError> {
+    let mut query_string = format!("?key={}&id={}&passlist=0&format=json", api_key, stop_id);
+    if direction.is_some() {
+        query_string.push_str(&format!("&direction={}", direction.unwrap()));
+    }
+    let res = ureq::get(&[server_url(), "/v2/departureBoard".to_string()].join(""))
+        .query_str(&query_string)
+        .call();
+    if !res.ok() {
+        return Err(PublicTransportError{
+            message: "Failed while making public transport API call".to_string()
+        });
+    }
+    let bytes = match res.into_string() {
+        Ok(text) => text.into_bytes(),
+        Err(_e) => {
+            return Err(PublicTransportError{
+                message: "Failed while making public transport API call".to_string()
+            });
+        }
+    };
+    return Ok(bytes);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use mockito::{mock, Matcher};
+
+    #[test]
+    fn test_public_transport() {
+        let api_key = "SOME_KEY".to_string();
+        let stop_id = "740025692".to_string();
+        let direction = "740015569".to_string();
+        let mock = mock("GET", Matcher::Regex(format!(r"^/v2/departureBoard\?key={}&id={}&passlist=0&format=json&direction={}$", api_key, stop_id, direction).to_string()))
+            .with_header("content-type", "application/json; charset=utf-8")
+            .with_status(200)
+            .with_body_from_file("files/public_transport.json")
+            .create();
+        let public_transport_response = get_public_transport(api_key, stop_id, Some(direction));
+        mock.assert();
+        assert!(public_transport_response.is_ok());
+
+        let public_transport = public_transport_response.unwrap();
+        assert_eq!(25, public_transport.len());
+
+        let departure = public_transport.get(0).unwrap();
+        assert_eq!("6", departure.number);
+        assert_eq!("Göteborg Temperaturgatan", departure.stop);
+        assert_eq!("Kortedala Aprilgatan (Göteborg kn)", departure.direction);
+        assert_eq!("2020-05-07T18:50:00+00:00", departure.time.to_rfc3339());
+        assert_eq!("2020-05-07T18:52:00+00:00", departure.real_time.unwrap().to_rfc3339());
+    }
+
+    #[test]
+    fn test_skip_direction_if_not_provided() {
+        let api_key = "SOME_KEY".to_string();
+        let stop_id = "740025692".to_string();
+        let mock = mock("GET", Matcher::Regex(format!(r"^/v2/departureBoard\?key={}&id={}&passlist=0&format=json$", api_key, stop_id).to_string()))
+            .with_header("content-type", "application/json; charset=utf-8")
+            .with_status(200)
+            .with_body_from_file("files/public_transport.json")
+            .create();
+        let _ = get_public_transport(api_key, stop_id, None);
+        mock.assert();
+    }
+}
