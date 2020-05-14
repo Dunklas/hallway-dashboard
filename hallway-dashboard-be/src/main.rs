89eb9d1cb416d1cff49ae8361b5145da515cde56
Rickard Andersson <dunklas@gmail.com>
2020-05-14 20:32:34 +0200

    Perform work in parallel using tokios join function

--- hallway-dashboard-be/src/main.rs
+++ hallway-dashboard-be/src/main.rs
@@ -4,7 +4,7 @@ use std::str::FromStr;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
 use simple_logger;
-use log::{self, warn};
+use log::{self, warn, info};
 use serde::{Deserialize, Serialize};
 use rusoto_core::{Region, ByteStream};
 use rusoto_s3::{S3, S3Client, PutObjectRequest};
@@ -26,26 +26,33 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 fn handle_request(_e: EmptyEvent, _c: Context) -> Result<EmptyOutput, HandlerError> {
-    let aws_region = Region::from_str(
-        &env::var("AWS_REGION").unwrap()
-    ).unwrap();
-    let storage_bucket_name = env::var("STORAGE_BUCKET").unwrap();
-    let s3_client = S3Client::new(aws_region);
-
     let mut run_time = Runtime::new()
         .expect("Failed to create Tokio runtime");
-
-    let dark_sky_api_key = env::var("DARK_SKY_API_KEY").unwrap();
-    run_time.block_on(get_and_store_weather(dark_sky_api_key, &s3_client, &storage_bucket_name));
-
-    let trafiklab_api_key = env::var("TRAFIKLAB_API_KEY").unwrap();
-    let stop_id = env::var("PUBLIC_TRANSPORT_STOP").unwrap();
-    let direction = env::var("PUBLIC_TRANSPORT_DIRECTION").ok();
-    run_time.block_on(get_and_store_public_transport(trafiklab_api_key, stop_id, direction, &s3_client, &storage_bucket_name));
+    run_time.block_on(inner_handle_request());
     Ok(EmptyOutput{})
 }
 
-async fn get_and_store_weather(dark_sky_api_key: String, s3_client: &S3Client, bucket_name: &String) {
+async fn inner_handle_request() {
+    let storage_bucket_name = env::var("STORAGE_BUCKET").unwrap();
+    let dark_sky_api_key = env::var("DARK_SKY_API_KEY").unwrap();
+    let trafiklab_api_key = env::var("TRAFIKLAB_API_KEY").unwrap();
+    let stop_id = env::var("PUBLIC_TRANSPORT_STOP").unwrap();
+    let direction = env::var("PUBLIC_TRANSPORT_DIRECTION").ok();
+    // TODO: Should do something with these values
+    let (_first, _second) = tokio::join!(
+        tokio::spawn(get_and_store_weather(dark_sky_api_key, get_aws_region(), storage_bucket_name.clone())),
+        tokio::spawn(get_and_store_public_transport(trafiklab_api_key, stop_id, direction, get_aws_region(), storage_bucket_name.clone()))
+    );
+}
+
+fn get_aws_region() -> Region {
+    return Region::from_str(
+        &env::var("AWS_REGION").unwrap()
+    ).unwrap();
+}
+
+async fn get_and_store_weather(dark_sky_api_key: String, aws_region: Region, bucket_name: String) {
+    info!("Start fetching weather");
     let weather = match weather::get_weather_forecast(dark_sky_api_key) {
         Ok(weather) => weather,
         Err(e) => {
@@ -61,6 +68,7 @@ async fn get_and_store_weather(dark_sky_api_key: String, s3_client: &S3Client, b
         }
     };
     let put_req = create_put_request(bucket_name.to_string(), "weather".to_string(), bytes);
+    let s3_client = S3Client::new(aws_region);
     match s3_client.put_object(put_req).await {
         Ok(res) => res,
         Err(e) => {
@@ -68,9 +76,11 @@ async fn get_and_store_weather(dark_sky_api_key: String, s3_client: &S3Client, b
             return;
         }
     };
+    info!("Stored weather data successfully");
 }
 
-async fn get_and_store_public_transport(trafiklab_api_key: String, stop_id: String, direction: Option<String>, s3_client: &S3Client, bucket_name: &String) {
+async fn get_and_store_public_transport(trafiklab_api_key: String, stop_id: String, direction: Option<String>, aws_region: Region, bucket_name: String) {
+    info!("Start fetching public transport departures");
     let public_transport = match public_transport::get_public_transport(trafiklab_api_key, stop_id, direction) {
         Ok(public_transport) => public_transport,
         Err(e) => {
@@ -86,6 +96,7 @@ async fn get_and_store_public_transport(trafiklab_api_key: String, stop_id: Stri
         }
     };
     let put_req = create_put_request(bucket_name.to_string(), "public_transport".to_string(), bytes);
+    let s3_client = S3Client::new(aws_region);
     match s3_client.put_object(put_req).await {
         Ok(res) => res,
         Err(e) => {
@@ -93,6 +104,7 @@ async fn get_and_store_public_transport(trafiklab_api_key: String, stop_id: Stri
             return;
         }
     };
+    info!("Stored public transport departures successfully");
 }
 
 fn create_put_request(bucket_name: String, key: String, body: Vec<u8>) -> PutObjectRequest {
