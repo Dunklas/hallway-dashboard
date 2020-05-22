809d5b157f37a0612767b691fb4e7f3feff400b6
Rickard Andersson <dunklas@gmail.com>
2020-05-22 22:47:37 +0200

    Wait for http requests

--- hallway-dashboard-fe/cypress/integration/basic.js
+++ hallway-dashboard-fe/cypress/integration/basic.js
@@ -6,16 +6,17 @@ describe('Hallway Dashboard', () => {
             method: 'GET',
             url: '**/weather',
             response: 'fixture:weather'
-        });
+        }).as('weather');
         cy.route({
             method: 'GET',
             url: '**/public_transport',
             response: 'fixture:public-transport'
-        });
+        }).as('public-transport');
     });
 
     it('Shows some data correct', () => {
         cy.visit('localhost:3000');
+        cy.wait(['@weather', '@public-transport']);
         cy.get('[data-test="departure-time"]')
           .first()
           .contains('22:54');
