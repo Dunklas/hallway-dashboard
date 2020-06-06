e24daa4b52392605147bc48b3a5218b6b003d125
Rickard Andersson <dunklas@gmail.com>
2020-06-06 13:23:47 +0200

    Fix formatting

--- hallway-dashboard-fe/src/component/Weather/WeatherBar.jsx
+++ hallway-dashboard-fe/src/component/Weather/WeatherBar.jsx
@@ -8,32 +8,32 @@ import styles from './WeatherBar.module.css';
 
 const WeatherBar = (props) => {
 
-	const weatherForecasts = props.weather
-		.map(weatherForecast => (
-			<WeatherForecast
-				key={weatherForecast.time.valueOf()}
-				weatherForecast={weatherForecast} />
+  const weatherForecasts = props.weather
+    .map(weatherForecast => (
+      <WeatherForecast
+        key={weatherForecast.time.valueOf()}
+        weatherForecast={weatherForecast} />
 
-		))
+    ))
 
-	return (
-		<>
-			<div className={styles.main}>
-				<div className={styles.weatherContainer}>
-					{weatherForecasts}
-				</div>
-				<div className={styles.weatherAttribution}>
-					<a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.weatherContainer}>
+          {weatherForecasts}
+        </div>
+        <div className={styles.weatherAttribution}>
+          <a href="https://darksky.net/poweredby/">Powered by Dark Sky</a>
+        </div>
+      </div>
+    </>
+  );
 }
 
 WeatherBar.propTypes = {
-	weather: PropTypes.arrayOf(
-		weatherForecast
-	).isRequired,
+  weather: PropTypes.arrayOf(
+    weatherForecast
+  ).isRequired,
 }
 
 export default WeatherBar;
